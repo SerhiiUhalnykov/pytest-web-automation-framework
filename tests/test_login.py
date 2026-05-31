70a326af2cd33794cda368af7ef07dc87593c0a6
Serhii Uhalnykov <serhii.uhalnykov@gmail.com>
2026-05-31 17:15:17 +0300

    test: add login/logout cases

---
+++ tests/test_login.py
@@ -0,0 +1,55 @@
+import pytest
+import allure
+from playwright.sync_api import Page
+
+from pages.login_page import LoginPage
+from pages.secure_page import SecurePage
+from utils.config import Users
+
+INVALID_LOGIN_CASES = [
+    pytest.param("", Users.STAN_PASS, id="empty-user"),
+    pytest.param("wronguser", Users.STAN_PASS, id="wrong-user"),
+    pytest.param("wronguser", "wrongpass", id="wrong-user-pass"),
+    pytest.param("", "", id="empty-user-pass"),
+]
+INVALID_PASS_CASES = [
+    pytest.param(Users.STAN_USER, "", id="empty-pass"),
+    pytest.param(Users.STAN_USER, "wrongpass", id="wrong-pass"),
+]
+
+@allure.severity(allure.severity_level.CRITICAL)
+@pytest.mark.smoke
+def test_full_login_logout_valid(page: Page) -> None:
+
+    login_pg = LoginPage(page)
+    login_pg.open()
+    login_pg.is_loaded()
+        
+    login_pg.login(Users.STAN_USER, Users.STAN_PASS)
+    secure_pg = SecurePage(page)
+    secure_pg.is_loaded()
+    secure_pg.check_success_popup()
+
+    secure_pg.logout()
+    login_pg = LoginPage(page)
+    login_pg.is_loaded()
+
+@pytest.mark.parametrize("username,password", INVALID_LOGIN_CASES)
+def test_login_invalid_username(page: Page, username: str, password: str):
+
+    login_pg = LoginPage(page)
+    login_pg.open()
+    login_pg.is_loaded()
+
+    login_pg.login(username, password)
+    login_pg.check_invalid_user_err()
+
+@pytest.mark.parametrize("username,password", INVALID_PASS_CASES)
+def test_login_invalid_password(page: Page, username: str, password: str): 
+    
+    login_pg = LoginPage(page)
+    login_pg.open()
+    login_pg.is_loaded()
+
+    login_pg.login(username, password)
+    login_pg.check_invalid_pass_err()
