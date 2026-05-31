d6a36eb57a9e8850598ee316b3b01626e04c6366
Serhii Uhalnykov <serhii.uhalnykov@gmail.com>
2026-05-31 17:12:52 +0300

    feat: add LoginPage object

---
+++ pages/login_page.py
@@ -0,0 +1,45 @@
+import allure
+from playwright.sync_api import Page, expect
+
+from pages.base_page import BasePage
+
+class LoginPage(BasePage):
+    PATH: str = "/login"
+
+    def __init__(self, page: Page) -> None:
+        super().__init__(page)
+
+        self._username_input = self._page.get_by_role("textbox", name="Username")
+        self._password_input = self._page.get_by_role("textbox", name="Password")
+        self._login_btn = self._page.get_by_role("button", name="Login")
+
+        self._invalid_err = self._page.locator("#flash")
+
+    @allure.step("Check login page is loaded")
+    def is_loaded(self) -> None:
+        
+        heading = self._page.get_by_role("heading", name="Login Page")
+
+        expect(heading).to_be_visible()
+        expect(self._username_input).to_be_visible()
+        expect(self._password_input).to_be_visible()
+        expect(self._login_btn).to_be_visible()
+    
+    @allure.step("Perfom Login")
+    def login(self, username: str, password: str) -> None:
+        
+        self._username_input.fill(username)
+        self._password_input.fill(password)
+        self._login_btn.click()
+    
+    @allure.step("Check invalid username error visibility")
+    def check_invalid_user_err(self) -> None:
+
+        expect(self._invalid_err).to_contain_text("Your username is invalid!")
+        expect(self._invalid_err).to_be_visible()
+    
+    @allure.step("Check invalid password error visibility")
+    def check_invalid_pass_err(self) -> None:
+
+        expect(self._invalid_err).to_contain_text("Your password is invalid!")
+        expect(self._invalid_err).to_be_visible()
