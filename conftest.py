4095b91fcab6a0b89b91c928250afc516a25818d
Serhii Uhalnykov <serhii.uhalnykov@gmail.com>
2026-06-07 18:56:48 +0300

    refactor: change reporting fixtures, move them into fixtures/

--- conftest.py
+++ conftest.py
@@ -1,33 +1,16 @@
 from typing import Iterator
-from pathlib import Path
 
-import allure
 import pytest
 from playwright.sync_api import Browser, BrowserContext, Page, sync_playwright
 
 from utils.logger import get_logger
-from utils.config import BROWSER_NAME, ARTIFACTS_DIR
+from utils.config import BROWSER_NAME
 
 logger = get_logger(__name__)
 
-@pytest.hookimpl(tryfirst=True, hookwrapper=True)
-def pytest_runtest_makereport(item: pytest.Item):
-    """Hook that generates a report each test for failure handling"""
-
-    outcome = yield
-    report: pytest.TestReport = outcome.get_result()
-    setattr(item, "rep_" + report.when, report)
-
-@pytest.fixture(scope="session", autouse=True)
-def artifacts_dirs() -> dict[str, Path]:
-    """Directory creation for report files storing"""
-
-    traces_dir = Path(ARTIFACTS_DIR) / "traces"
-    screenshots_dir = Path(ARTIFACTS_DIR) / "screenshots"
-    traces_dir.mkdir(parents=True, exist_ok=True)
-    screenshots_dir.mkdir(parents=True, exist_ok=True)
-
-    return {"traces": traces_dir, "screenshots": screenshots_dir}
+pytest_plugins = [
+    "fixtures.reporting_fixtures",
+]
 
 @pytest.fixture(scope="session")
 def browser() -> Iterator[Browser]:
@@ -40,51 +23,20 @@ def browser() -> Iterator[Browser]:
         browser.close()
 
 @pytest.fixture(scope="function")
-def context(browser: Browser, 
-            request: pytest.FixtureRequest, 
-            artifacts_dirs: dict[str, Path]
-            ) -> Iterator[BrowserContext]:
-    """Playwright context creation with tracing saved on test fail"""
+def context(browser: Browser) -> Iterator[BrowserContext]:
+    """Playwright context creation with tracing"""
 
     context = browser.new_context()
+    # NOTE: tracing stoppage is handled by the save_attach_results fixture
     context.tracing.start(screenshots=True, snapshots=True, sources=True)
 
     yield context
-
-    if request.node.rep_call.failed:
-        logger.info(f"Saving trace on failed test {request.node.name}")
-
-        trace_path = artifacts_dirs["traces"] / f"{request.node.name}.zip"
-        context.tracing.stop(path=str(trace_path))
-        allure.attach.file(
-            str(trace_path), 
-            name="playwright_trace",
-            attachment_type=allure.attachment_type.ZIP,
-        )
-    else:
-        context.tracing.stop()
-    
     context.close()
 
 @pytest.fixture(scope="function")
-def page(context: BrowserContext, 
-         request: pytest.FixtureRequest,
-         artifacts_dirs: dict[str, Path]
-         ) -> Iterator[Page]:
-    """Playwright page creation with screenshot saved on test fail"""
+def page(context: BrowserContext) -> Iterator[Page]:
+    """Playwright page creation"""
 
     page = context.new_page()
-
     yield page
-
-    if request.node.rep_call.failed:
-        logger.info(f"Saving screenshot on failed test {request.node.name}")
-
-        screenshot_path = artifacts_dirs["screenshots"] / f"{request.node.name}.png"
-        page.screenshot(path=str(screenshot_path), full_page=True)
-        allure.attach.file(
-            str(screenshot_path), 
-            name="failure_screenshot",
-            attachment_type=allure.attachment_type.PNG,
-        )
     page.close()
---
+++ fixtures/reporting_fixtures.py
@@ -0,0 +1,81 @@
+from typing import Iterator
+from pathlib import Path
+
+import allure
+import pytest
+from playwright.sync_api import BrowserContext, Page
+
+from utils.logger import get_logger
+from utils.config import BROWSER_NAME, ARTIFACTS_DIR
+
+logger = get_logger(__name__)
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item: pytest.Item):
+    """Hook that generates a report status each test for failure handling"""
+
+    outcome = yield
+    report: pytest.TestReport = outcome.get_result()
+    setattr(item, "rep_" + report.when, report)
+
+@pytest.fixture(scope="session", autouse=True)
+def artifacts_dir() -> Path:
+    """Create a directory to store test artifacts"""
+
+    art_dir = Path(ARTIFACTS_DIR) / BROWSER_NAME
+    art_dir.mkdir(parents=True, exist_ok=True)
+    logger.info(f"Artifacts dir: {art_dir}")
+
+    return art_dir
+
+@pytest.fixture(scope="function")
+def artifacts_path(artifacts_dir: Path, 
+                   request: pytest.FixtureRequest
+                   ) -> dict[str, Path]:
+    """Create names and paths for test artifacts"""
+
+    trace_path = artifacts_dir / (request.node.name + '.zip')
+    screenshot_path = artifacts_dir / (request.node.name + '.png')
+
+    return {"trace": trace_path, "screenshot": screenshot_path}
+
+@pytest.fixture(autouse=True)
+def save_attach_results(page: Page, 
+                        context: BrowserContext, 
+                        request: pytest.FixtureRequest,
+                        artifacts_path: dict[str, Path]
+                        ) -> Iterator[None]:
+    """Save test artifacts on fail and attach them to Allure report.
+    
+    Args:
+        page (Page): Playwright page object for screenshot capture;
+        context (BrowserContext): Playwright context object for tracing;
+        request (FixtureRequest): pytest request object for test status;
+        artifacts_path (dict[str, Path]): artifacts files path.
+    """
+
+    yield
+    # NOTE: the fixture controls test context object tracing stoppage
+    # since stopping tracing and saving the file cannot be separated.
+    if request.node.rep_call.failed:
+
+        screenshot_path = artifacts_path["screenshot"]
+        trace_path = artifacts_path["trace"]
+        logger.info("Saving artifacts on failed test: "
+                    f"{screenshot_path.name}, {trace_path.name}")
+
+        page.screenshot(path=str(screenshot_path), full_page=True)
+        context.tracing.stop(path=str(trace_path))
+
+        allure.attach.file(
+            str(screenshot_path), 
+            name="failure_screenshot",
+            attachment_type=allure.attachment_type.PNG,
+        )
+        allure.attach.file(
+            str(trace_path), 
+            name="playwright_trace",
+            attachment_type=allure.attachment_type.ZIP,
+        )
+    else:
+        context.tracing.stop()
